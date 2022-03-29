037ad64c69c6c48eb6e2f5c5ee8c404e9b6d3e65
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-29 10:43:35 -0700

    commit tree 8d8e2260e70bdbd148ac380102ce8342d38ceedc

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,24 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    #render json: @birds
+    #fully written out
+    #render json: birds.to_json(except: [:created_at, :updated_at])
+    
+    render json: birds, only: [:id, :name, :species]
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+    # create a new hash out of 3 keys, assigning the keys manually with the attributes
+    # render json: {id: bird.id, name: bird.name, species: bird.species }
+    
+    # slice returns a new hash with only the keys that are passed into slice
+      render json: bird.slice(:id, :name, :species)
+    else 
+      render json: {message: 'Bird not found' }
+    end
+  end
+
 end
